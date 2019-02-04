b34d51fc8322a4020315604a5bb1aac165a6b60c
SAUVALLE Damien <damien.sauvalle.etu@univ-lille.fr>
2019-02-04 10:32:27 +0100

    Controller Int done

---
+++ src/test/java/com/miage/altea/tp/trainer_api/controller/TrainerControllerIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.miage.altea.tp.trainer_api.controller;
+
+import com.miage.altea.tp.trainer_api.bo.Trainer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TrainerControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TrainerController controller;
+
+    @Test
+    public void trainerController_shouldBeInstanciated() {
+        assertNotNull(controller);
+    }
+
+    @Test
+    public void getTrainer_withNameAsh_shouldReturnAsh() {
+        var ash = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/Ash", Trainer.class);
+        assertNotNull(ash);
+        assertEquals("Ash", ash.getName());
+        assertEquals(1, ash.getTeam().size());
+
+        assertEquals(25, ash.getTeam().get(0).getPokemonType());
+        assertEquals(18, ash.getTeam().get(0).getLevel());
+    }
+
+    @Test
+    public void getAllTrainers_shouldReturnAshAndMisty() {
+        var trainers = this.restTemplate.getForObject("http://localhost:" + port + "/trainers/", Trainer[].class);
+        assertNotNull(trainers);
+        assertEquals(2, trainers.length);
+
+        assertEquals("Ash", trainers[0].getName());
+        assertEquals("Misty", trainers[1].getName());
+    }
+}
